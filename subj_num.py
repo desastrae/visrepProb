75c2d7d65d52ef74e632f435f94306329015b775
desastrae <anastasiaborisenkov@gmail.com>
2022-08-22 20:49:49 +0200

    added readibility to pie chart

--- subj_num.py
+++ subj_num.py
@@ -22,31 +22,33 @@ def eval_distribution(read_path_file):
 
     print(len(data_dict.keys()), '\n', data_dict)
 
-    # make percentages
-    sum_val = sum(list(data_dict.values()))
-    for key, val in data_dict.items():
-        data_perc_dict[key] = val / sum_val
-
-    print(data_perc_dict)
-
-    return data_dict, data_perc_dict
+    return data_dict
 
 
 def plot_results_pie(enc_task, data_dict, path_save):  # , maj_cl_val):
     # wasn't able to extract normalized Layer for text-model; temporary fix, drop normalized layer for v_model
     # data_dict_v.drop(data_dict_v.tail(1).index, inplace=True)
-    sorted_data_dict = dict(sorted(data_dict.items(), key=lambda x:x[1], reverse=True))
+    sorted_data_dict = dict(sorted(data_dict.items(), key=lambda x: x[1], reverse=True))
 
     labels = list(sorted_data_dict.keys())
     results = list(sorted_data_dict.values())
 
     fig1, ax1 = plt.subplots()
-    ax1.pie(results, labels=labels, autopct='%1.1f%%',
-            shadow=True, startangle=90)
+    ax1.pie(results, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    total = sum(results)
+    plt.legend(
+        loc='best',
+        labels=['%s, %1.1f%%' % (
+            l, (float(s) / total) * 100) for l, s in zip(labels, results)],
+        prop={'size': 10},
+        bbox_to_anchor=(0.81, 0.5),
+        # bbox_transform=fig1.transFigure
+    )
 
-    # plt.show()
-    plt.savefig(path_save + 'eval_' + enc_task + '.png')
+    plt.tight_layout()
+    plt.show()
+    # plt.savefig(path_save + 'eval_' + enc_task + '.png')
 
     # x = np.arange(len(labels))  # the label locations
     # width = 0.35  # the width of the bars
@@ -72,7 +74,8 @@ def plot_results_pie(enc_task, data_dict, path_save):  # , maj_cl_val):
 
 
 if __name__ == '__main__':
-    eval_dict, eval_perc_dict = eval_distribution('/local/anasbori/xprobe/de/subj_number/subjnum_tr_out.csv')
-    # plot_results_v_vs_t('SUBJ', eval_dict)
-    plot_results_pie('SUBJ', eval_perc_dict, '/local/anasbori/visrepProb/task_encs/')
+    # eval_dict, eval_perc_dict = eval_distribution('/local/anasbori/xprobe/de/subj_number/subjnum_tr_out.csv')
+    eval_dict = eval_distribution('/home/anastasia/PycharmProjects/xprobe/de/subj_number/Verwaltung_tr.txt')
+    # plot_results_pie('SUBJ', eval_perc_dict, '/local/anasbori/visrepProb/task_encs/')
+    plot_results_pie('SUBJ', eval_dict, '/local/anasbori/visrepProb/task_encs/')
 
