a373efbb468152aeddcfe6cea9bc580b781c4ff8
desastrae <anastasiaborisenkov@gmail.com>
2022-08-24 21:12:03 +0200

    more details

--- subj_num.py
+++ subj_num.py
@@ -15,10 +15,12 @@ def eval_distribution(read_path_file):
             feat_list = list()
 
             doc = nlp(line)
-            sb_num_list = [(token.dep_, token.morph.get('Number')[0]) for token in doc if token.dep_ in ['sb'] and
-                           token.morph.get('Number')]
-            empty_morph_list = list(filter(lambda dep_num: dep_num[1] is [], sb_num_list))
-            data_dict[tuple(sb_num_list)] += 1
+            sb_num_list = [(token.dep_, token.morph.get('Number')[0]) for token in doc if token.dep_ in
+                           ['sb', 'oa', 'oc', 'og'] and token.morph.get('Number')]
+            sub_ob_num_list = list(('o', obj[1]) if obj[0] in ['oa', 'oc', 'og'] else obj for obj in sb_num_list)
+
+            # empty_morph_list = list(filter(lambda dep_num: dep_num[1] is [], sb_num_list))
+            data_dict[tuple(set(sub_ob_num_list))] += 1
 
     print(len(data_dict.keys()), '\n', data_dict)
 
@@ -33,7 +35,8 @@ def plot_results_pie(enc_task, data_dict, path_save):  # , maj_cl_val):
     labels = list(sorted_data_dict.keys())
     results = list(sorted_data_dict.values())
 
-    fig1, ax1 = plt.subplots()
+    fig1 = plt.figure(figsize=(10, 10))
+    ax1 = fig1.subplots()
     ax1.pie(results, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     total = sum(results)
@@ -41,41 +44,28 @@ def plot_results_pie(enc_task, data_dict, path_save):  # , maj_cl_val):
         loc='best',
         labels=['%s, %1.1f%%' % (
             l, (float(s) / total) * 100) for l, s in zip(labels, results)],
-        prop={'size': 10},
+        prop={'size': 13},
         bbox_to_anchor=(0.81, 0.5),
         # bbox_transform=fig1.transFigure
     )
-
+    plt.title('Data evaluation of task ' + enc_task)
     plt.tight_layout()
-    plt.show()
-    # plt.savefig(path_save + 'eval_' + enc_task + '.png')
-
-    # x = np.arange(len(labels))  # the label locations
-    # width = 0.35  # the width of the bars
-    #
-    # fig, ax = plt.subplots()
-    # rects1 = ax.bar(x - width / 2, results, width)  ## , label='ToDo')
-    #
-    # # Add some text for labels, title and custom x-axis tick labels, etc.
-    # ax.set_xlabel('Pairs')
-    # ax.set_ylabel('Distribution in Percentage')
-    # ax.set_title('Dataset evalutation for the task ' + enc_task)
-    # ax.set_xticks(x, labels)
-    # ax.legend()
-    #
-    # ax.bar_label(rects1, padding=3)
-    #
-    # fig.tight_layout()
-    # # plt.ylim([0.45, 1.0])
-    #
     # plt.show()
-    # plt.savefig(path_save + 'v_vs_t_results_' + enc_task + '_' + str(size) + '.png')
+    plt.savefig(path_save + 'eval_' + enc_task + '.png')
+
     plt.close()
 
 
 if __name__ == '__main__':
-    # eval_dict, eval_perc_dict = eval_distribution('/local/anasbori/xprobe/de/subj_number/subjnum_tr_out.csv')
-    eval_dict = eval_distribution('/home/anastasia/PycharmProjects/xprobe/de/subj_number/Verwaltung_tr.txt')
-    # plot_results_pie('SUBJ', eval_perc_dict, '/local/anasbori/visrepProb/task_encs/')
+    # test locally
+    # eval_dict = eval_distribution('/home/anastasia/PycharmProjects/xprobe/de/subj_number/Verwaltung_tr.txt')
+    # plot_results_pie('SUBJ', eval_dict, '/home/anastasia/PycharmProjects/visrepProb/task_encs/')
+
+    # SUBJ
+    eval_dict = eval_distribution('/local/anasbori/xprobe/de/subj_number/subjnum_out_clean_uniq.csv')
     plot_results_pie('SUBJ', eval_dict, '/local/anasbori/visrepProb/task_encs/')
+
+    # OBJ
+    eval_dict = eval_distribution('/local/anasbori/xprobe/de/obj_number/objnum_out_uniq.csv')
+    plot_results_pie('OBJ', eval_dict, '/local/anasbori/visrepProb/task_encs/')
 
