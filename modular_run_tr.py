ad892a879609cb7aab1e7e9e3b9be92b77e7604b
desastrae <anastasiaborisenkov@gmail.com>
2023-07-30 17:09:08 +0200

    bau

--- modular_run_tr.py
+++ modular_run_tr.py
@@ -629,7 +629,8 @@ class VisRepEncodings:
                 # print(np.where(np.isnan(test_features)))
 
                 # print(test_labels[100])
-                print(len(test_features), len(test_labels))
+                print('train: ', len(train_features), len(train_labels))
+                print('test: ', len(test_features), len(test_labels))
 
                 mlp_clf = MLPClassifier(random_state=1, max_iter=300).fit(train_features, train_labels)
                 #  print('mlp_clf.predict: ', mlp_clf.predict(test_features[:5, :]))
