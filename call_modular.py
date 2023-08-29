e35564b64233c5b2ddd867cd0099869e73423d67
desastrae <anastasiaborisenkov@gmail.com>
2023-08-29 15:13:17 +0200

    obtain noise classifier + results

--- call_modular.py
+++ call_modular.py
@@ -296,6 +296,7 @@ if __name__ == '__main__':
                     if saved_classifier:
                         # data_size_list = [10000]  # , 1000]
                         data_size_list = config_dict['dataset_size'][:1]
+                        print(data_size_list)
                         path_in_test = path_server_o_lokal + config_dict['data_path_in'] + task + '/'
                         path_out = path_server_o_lokal + config_dict['data_path_in'] + task + '/' + m_type + '/'
 
